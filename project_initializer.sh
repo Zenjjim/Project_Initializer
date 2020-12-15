3c01b2c6ac6eb029418c807218830ecee21d3122
Zaim Imran <zaim.imran@gmail.com>
2020-12-15 23:24:42 +0100

    Better feedback

--- project_initializer.sh
+++ project_initializer.sh
@@ -18,20 +18,26 @@ elif [ -d $1 ]
 then 
     echo "${RED}Project already exists"
 else
-    echo "Creating repo"
+    echo -ne "Creating project:             0%"\\r
     python3 $ABSOLUTE_PATH_TO_INITIALIZER/Project_Initializer/project_initializer.py $1 $GITHUB_API_TOKEN
-
+    echo -ne "Creating project: ##          20%"\\r
     cd $1
+    echo -ne "Creating project: ###         30%"\\r
     
-    echo "README:" > README.md
+    echo -ne "README:" > README.md
     echo "Ignore files:" > .gitignore
+    echo -ne "Creating project: ####        50%"\\r
 
-    echo "Initialing project"
     git init &> /dev/null
+    echo -ne "Creating project: ######      60%"\\r
     git add . &> /dev/null
+    echo -ne "Creating project: #######     70%"\\r
     git commit -m "Create project" &> /dev/null
+    echo -ne "Creating project: ########    80%"\\r
     git remote add origin https://github.com/$GITHUB_USERNAME/$1.git &> /dev/null
+    echo -ne "Creating project: #########  90%"\\r
     git push -u origin master &> /dev/null
+    echo "Creating project: ########## 100%"
 
     echo "${GREEN}Project $1 is created. Happy coding!"
     xdg-open https://github.com/$GITHUB_USERNAME/$1
